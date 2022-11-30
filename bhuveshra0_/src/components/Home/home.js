e67d1dce830ff85a088eaa41a23dbb8f0ff81c22
bhuvesh <bhuvesh@createbytes.com>
2022-12-01 00:48:53 +0530

    about in progress

--- bhuveshra0_/src/components/Home/home.js
+++ bhuveshra0_/src/components/Home/home.js
@@ -4,8 +4,24 @@ import styles from '../Home/home.module.scss'
 
 function body() {
   return (
-    <div id={styles.container}>
-    </div>
+    <div id={styles.container} className="p-0 p-md-5">
+      <div className="container m-auto">
+        <h4 className='display-5' id={styles.letterGap}>HELLO !</h4>
+        <h3 className='d-inline-block display-6' id={styles.letterGap}>I  AM &nbsp;</h3>
+        <h3 className='d-inline-block display-4  m-auto text-start ' id={styles.nametag}>BHUVESH RAO </h3>
+        <h1 className="display-1" id={styles.letterGapFE}>FRONTEND DEVELOPER</h1>
+
+        <div className='d-flex justify-content-center justify-content-md-start align-items-center' id={styles.btnholder}>
+          <a className='' id={styles.btnAnchor} href="/" target="_blank">
+            <span className={styles.btnSpan}> </span>
+            <span className={styles.btnSpan}> </span>
+            <span className={styles.btnSpan}> </span>
+            <span className={styles.btnSpan}> </span>
+            About
+          </a>
+        </div>
+      </div>
+    </div >
   )
 }
 
