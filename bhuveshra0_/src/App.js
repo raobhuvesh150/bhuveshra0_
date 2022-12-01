b29c307dc426c349e5fea4db566d1c7e968d2a15
bhuvesh <bhuvesh@createbytes.com>
2022-12-01 21:12:14 +0530

    dock

--- bhuveshra0_/src/App.js
+++ bhuveshra0_/src/App.js
@@ -4,7 +4,7 @@ import Header from './components/Header/Header';
 import Home from './components/Home/home';
 import Design from './components/Design/Design'
 import Navbar from './components/Navbar/Navbar';
-
+import Navbar2 from './components/Navbar/Navbar2';
 
 function App() {
   return (
@@ -12,7 +12,6 @@ function App() {
       <div className="">
         <Header />
         <Home />
-        {/* <Navbar /> */}
       </div>
     </>
   );
--- bhuveshra0_/src/components/Navbar/Navbar2.js
+++ bhuveshra0_/src/components/Navbar/Navbar2.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Dock from "react-osx-dock";
+import Img1 from './woodenImages/img1.jpg'
+import Img2 from './woodenImages/img2.jpg'
+import Img3 from './woodenImages/img3.jpg'
+import Img4 from './woodenImages/img4.jpg'
+import Img5 from './woodenImages/img4.jpg'
+import Img6 from './woodenImages/img6.jpg'
+
+
 
 function Navbar2() {
-  return (
-    <div>
+    // const[size , setSize] = useState(800)
+    return (
+        <div className=''>
+            <Dock width={600} magnification={1} magnifyDirection="center">
+                {[Img1,Img2,Img3,Img4,Img5,Img6,Img1,Img2,Img3,Img4,Img5,Img6 ,Img1,Img2,Img3,Img4,Img5,Img6].map((item, index) => (
+                    <Dock.Item key={index} onClick={() => console.log(item)}>
+                        <img className='w-75' src={`${item}`} />
+                    </Dock.Item>
+                ))}
+            </Dock>
 
-    </div>
-  )
+        </div>
+    )
 }
 
 export default Navbar2
